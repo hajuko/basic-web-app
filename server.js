99f1dd8a8f58c3c26db2eb831cf8b0973cad3461
Julien Thoma <julien.thoma@reservix.de>
2016-06-17 21:27:21 +0200

    update

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
-/**
- * Created by julien on 17.06.16.
- */
+var express = require('express');
+var app = express();
+var request = require('request');
+var port = 1337;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port);
+
+app.get('/test-request', function(req, res) {
+    request('http://jsonplaceholder.typicode.com/posts/1', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(JSON.parse(body));
+        }
+    })
+});
+
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
+console.log('started at port: ' + port);
+
+module.exports = app;
